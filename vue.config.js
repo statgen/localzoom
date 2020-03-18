4af9ba65c182fcdbe3ad8f1cd09d80f3735545d0
Andy Boughton <abought@gmail.com>
2020-03-17 22:00:59 -0400

    Demonstrate using source-map-loader for a better LZ debugging experience

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,16 @@
-module.exports = { publicPath: './' };
+module.exports = {
+    chainWebpack: (config) => {
+        config.module
+            .rule('source-map-loader')
+            .test(/\.js$/)
+            .enforce('pre')
+            .use('source-map-loader')
+            .loader('source-map-loader')
+            .end();
+    },
+    configureWebpack: {
+        // Ensure that (decent) source maps are used, even in development
+        devtool: (process.env.NODE_ENV === 'development') ? 'eval-source-map' : 'source-map',
+    },
+    publicPath: './',
+};
