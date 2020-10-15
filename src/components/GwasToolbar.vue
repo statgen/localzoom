dda4d26d099e7a57b2ad7da5060e42b17f7c47dc
Andy Boughton <abought@gmail.com>
2020-10-15 16:03:17 -0400

    Show an error message (instead of silent failure) when study already added to plot

--- src/components/GwasToolbar.vue
+++ src/components/GwasToolbar.vue
@@ -30,7 +30,7 @@ export default {
         // Toolbar can optionally consider a list of studies already on plot
         study_names: {
             type: [Array, null],
-            default: null,
+            default: () => [],
         },
     },
     data() {
@@ -83,9 +83,14 @@ export default {
             this.$emit('select-range', config);
         },
         connectReader(reader, name) {
+            if (this.study_names.includes(name)) {
+                this.showMessage('A study with this name has already been added to the plot');
+                return;
+            }
             this.file_reader = reader;
             this.display_name = name;
             this.show_modal = true;
+            this.showMessage('');
         },
         activateBatchMode(regions) {
             this.batch_mode_active = true;
@@ -124,24 +129,24 @@ export default {
 <template>
   <div>
     <div class="row">
-      <div 
-        v-if="!batch_mode_active" 
+      <div
+        v-if="!batch_mode_active"
         class="col-sm-6">
         <div v-if="study_count < max_studies">
-          <tabix-file 
+          <tabix-file
             class="mr-1"
-            @ready="connectReader" 
+            @ready="connectReader"
             @fail="showMessage"/>
-          <b-dropdown 
-            text="Add from URL" 
+          <b-dropdown
+            text="Add from URL"
             variant="success">
             <div class="px-3">
-              <tabix-url 
-                @ready="connectReader" 
+              <tabix-url
+                @ready="connectReader"
                 @fail="showMessage"/>
             </div>
           </b-dropdown>
-          <adder-wizard 
+          <adder-wizard
             v-if="show_modal"
             :file_reader="file_reader"
             :file_name.sync="display_name"
@@ -149,89 +154,89 @@ export default {
             @close="show_modal = false"/>
         </div>
       </div>
-      <div 
-        v-if="!batch_mode_active" 
+      <div
+        v-if="!batch_mode_active"
         class="col-sm-6">
-        <div 
-          v-if="study_count" 
+        <div
+          v-if="study_count"
           class="d-flex justify-content-end">
-          <region-picker 
+          <region-picker
             :build="build"
             :max_range="max_region_size"
             search_url="https://portaldev.sph.umich.edu/api/v1/annotation/omnisearch/"
             @ready="selectRange"
             @fail="showMessage"/>
-          <batch-spec 
+          <batch-spec
             :max_range="max_region_size"
             class="ml-1"
             @ready="activateBatchMode"/>
         </div>
-        <b-dropdown 
-          v-else 
-          text="Plot options" 
+        <b-dropdown
+          v-else
+          text="Plot options"
           variant="info"
           class="float-right">
           <div class="px-3">
             <strong>Annotations</strong><br>
             <div class="form-check form-check-inline">
-              <input 
-                id="show-catalog" 
-                v-model="has_catalog" 
+              <input
+                id="show-catalog"
+                v-model="has_catalog"
                 class="form-check-input"
                 type="checkbox">
-              <label 
-                class="form-check-label" 
+              <label
+                class="form-check-label"
                 for="show-catalog">GWAS Catalog</label>
             </div>
             <div class="form-check form-check-inline">
-              <input 
-                id="show-credible-set" 
-                v-model="has_credible_sets" 
+              <input
+                id="show-credible-set"
+                v-model="has_credible_sets"
                 class="form-check-input"
                 type="checkbox">
-              <label 
-                class="form-check-label" 
+              <label
+                class="form-check-label"
                 for="show-credible-set">95% credible set</label>
             </div>
             <strong>Genome Build</strong><br>
             <div class="form-check">
-              <input 
-                id="build-37" 
-                v-model="build" 
+              <input
+                id="build-37"
+                v-model="build"
                 class="form-check-input"
-                type="radio" 
-                name="build" 
+                type="radio"
+                name="build"
                 value="GRCh37">
-              <label 
-                class="form-check-label" 
+              <label
+                class="form-check-label"
                 for="build-37">GRCh37</label>
             </div>
             <div class="form-check">
-              <input 
-                id="build-38" 
-                v-model="build" 
+              <input
+                id="build-38"
+                v-model="build"
                 class="form-check-input"
-                type="radio" 
-                name="build" 
+                type="radio"
+                name="build"
                 value="GRCh38">
-              <label 
-                class="form-check-label" 
+              <label
+                class="form-check-label"
                 for="build-38">GRCh38</label>
             </div>
           </div>
         </b-dropdown>
       </div>
 
-      <div 
-        v-if="batch_mode_active" 
+      <div
+        v-if="batch_mode_active"
         class="col-md-12">
-        <batch-scroller 
+        <batch-scroller
           :regions="batch_mode_regions"
           @navigate="selectRange"
           @cancel="batch_mode_active = false"/>
       </div>
-      <div 
-        v-if="message" 
+      <div
+        v-if="message"
         class="row">
         <div class="col-sm-12"><span :class="[message_class]">{{ message }}</span></div>
       </div>
